9d9c506ee494303815348ed769e7c7ec0d857a60
Tanzir Razzaque <44029202+TanzirR@users.noreply.github.com>
2025-05-04 02:48:12 +0600

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from "express"
+import path, {dirname} from "path"
+import { fileURLToPath } from "url"
+
+const app = express()
+const PORT = process.env.PORT || 8000
+
+// Get the file path from the URL of the current module
+const __filename = fileURLToPath(import.meta.url)
+// Get the directory name from the file path
+const __dirname = dirname(__filename)
+
+console.log("filename:", __filename)
+console.log("__dirname:", __dirname)
+
+// Middleware
+app.use(express.json()) //for post method 
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+// Serving up the HTML file from the /public directory
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+app.listen(PORT, ()=>{
+    console.log(`Server has started on port ${PORT}`)
+})
+
