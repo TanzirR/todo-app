d4a1545cb1c532765b738e0ed63a9260b2f3b6ea
Tanzir Razzaque <44029202+TanzirR@users.noreply.github.com>
2025-05-04 04:24:26 +0600

    Fixed errors

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ const db = new DatabaseSync(':memory:')
 db.exec(`
         CREATE TABLE users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT UNIQUE
+            username TEXT UNIQUE,
             password TEXT
         
         )
@@ -16,7 +16,7 @@ db.exec(`
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             user_id INTEGER, 
             task TEXT, 
-            completed BOOLEAN DEFAULT 0
+            completed BOOLEAN DEFAULT 0,
             FOREIGN KEY(user_id) REFERENCES users(id)
         )
     `)
