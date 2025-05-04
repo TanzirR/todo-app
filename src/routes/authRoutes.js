52e9c5af8599d4fb9ec07a199f39fa482bbd6d36
Tanzir Razzaque <44029202+TanzirR@users.noreply.github.com>
2025-05-05 04:15:12 +0600

    New users are added to db during sign up and their password is encrypted

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,7 +5,42 @@ import db from "../db.js"
 
 const router = express.Router()
 
-router.post("/register", (req, res) =>{})
+
+//Register a new user endpoint: /auth/register 
+router.post("/register", (req, res) =>{
+    //Get the username and password 
+    const {username, password} = req.body
+    
+    //encrypt the password 
+    const hasedPassword = bcrypt.hashSync(password, 8)
+
+    //should be in a separate server entity 
+    //save the new user and hased password "users" table, specifically the "username" and "password"
+    //column with default VALUES as ?. The username and hashedPassword from 
+    // result is passed as the VALUES
+    try{
+        const insertUser = db.prepare(`INSERT INTO users(username, password) VALUES (?,?)`)
+        const result = insertUser.run(username, hasedPassword)
+        //After having a user, create a default todo for them 
+        const defaultTodo = "Your first TODO!"
+        //Insert the todo into db
+        const insertTodo = db.prepare(`INSERT INTO todos(user_id, task) VALUES (?, ?)`)
+        //in .run() the user_id is needed which can be retrived from the result
+        //.lastInsertRowid -> get the id from the last entry of the user table 
+        insertTodo.run(result.lastInsertRowid, defaultTodo)
+
+        //create a token
+        const token = jwt.sign({id: result.lastInsertRowid},process.env.JWT_SECRET, {expiresIn: '24h'})
+
+        //converts the js object into JSON and sends it to the client  
+        res.json({token})
+         
+    } catch(err){
+        console.log(err.message)
+        res.sendStatus(503)
+    }
+    res.sendStatus(201) 
+})
 
 router.post('/login', (req, res)=>{})
 
