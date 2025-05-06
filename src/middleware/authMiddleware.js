032570e46cd7b0f44e2c4c92672d32dec8b3df39
Tanzir Razzaque <tanzir.razzaque@gmail.com>
2025-05-07 01:33:10 +0600

    Update authMiddleware.js

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -7,12 +7,12 @@ function authMiddleware(req, res, next){
     const token = req.headers['authorization']
 
     if (!token){
-        return res.status(401).send({message: "No token provided"})
+        return res.status(401).json({message: "No token provided"})
     }
     //Verify the token
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded)=>{
         if(err){
-            return res.status(401).send({message:"Invalid token"})
+            return res.status(401).json({message:"Invalid token"})
         }
         req.userId = decoded.id
         next()
