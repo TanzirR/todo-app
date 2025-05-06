7a4d14fab4284a8203eeea20e0946f2e96d5a2a8
Tanzir Razzaque <tanzir.razzaque@gmail.com>
2025-05-07 02:15:08 +0600

    Update todoRoutes.js

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -10,13 +10,35 @@ router.get('/', (req, res) => {
     res.json(todos)
 })
 
-//Create a new todo 
-router.post("/", (req, res)=>{})
+// Create a new todo
+router.post('/', (req, res) => {
+    const { task } = req.body
+    const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`)
+    const result = insertTodo.run(req.userId, task)
 
-//Update a todo 
-router.put("/:id", (res,req)=>{})
+    res.json({ id: result.lastInsertRowid, task, completed: 0 })
+})
 
-//Delete a todo
-router.delete("/:id", (req, res)=>{})
+// Update a todo
+router.put('/:id', (req, res) => {
+    const { completed } = req.body
+    const { id } = req.params
+    const { page } = req.query
+
+    const updatedTodo = db.prepare('UPDATE todos SET completed = ? WHERE id = ?')
+    updatedTodo.run(completed, id)
+
+    res.json({ message: "Todo completed" })
+})
+
+// Delete a todo
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    const userId = req.userId
+    const deleteTodo = db.prepare(`DELETE FROM todos WHERE id = ? AND user_id = ?`)
+    deleteTodo.run(id, userId)
+    
+    res.send({ message: "Todo deleted" })
+})
 
 export default router 
